de5c04f5b3c8478ef504d461d63dc29dd91990fd
Jeffery Candiloro <jeffery@astraaustralis.com.au>
2011-06-15 05:23:10 +0000

    Import of JEFFERY/Net-MySourceMatrix-0.04 from CPAN.
    gitpan-cpan-distribution: Net-MySourceMatrix
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         JEFFERY/Net-MySourceMatrix-0.04.tar.gz
    gitpan-cpan-author:       JEFFERY
    gitpan-cpan-maturity:     released

--- lib/Net/MySourceMatrix.pm
+++ lib/Net/MySourceMatrix.pm
@@ -5,7 +5,7 @@ use 5.008005;
 use SOAP::Lite;
 use MIME::Base64 qw(encode_base64);
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 sub new	{
 		
@@ -906,7 +906,7 @@ Net::MySourceMatrix - A Perl wrapper to the MySource Matrix(TM)/Squiz Matrix(TM)
 
 =head1 VERSION
 
-This documentation refers to version 0.03.
+This documentation refers to version 0.04.
 
 =head1 SYNOPSIS
 
